7b1b4716b9a158d10af9484a1c26c7fc94c3eccc
Jasmin-Mehrju <jasminmehrju19@gmail.com>
2025-03-07 08:58:30 +0100

    m

---
+++ iron_defender.py
@@ -0,0 +1,109 @@
+import os
+import pygame
+
+class Settings():
+    WINDOW = pygame.rect.Rect((0, 0, 1400, 800))
+    FPS = 60
+    DELTATIME = 1.0 / FPS
+    DIRECTIONS = {"right": pygame.math.Vector2(5, 0), 
+                  "left": pygame.math.Vector2(-5, 0), 
+                  "up": pygame.math.Vector2(0, -5), 
+                  "down": pygame.math.Vector2(0, 5)}
+    TITLE = "Iron Defender"
+    FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+    IMAGE_PATH = os.path.join(FILE_PATH, "images")
+    start_pos = pygame.math.Vector2(0, WINDOW.height // 2)
+
+
+class Iron_Man(pygame.sprite.Sprite):
+    def __init__(self, image_file, size, pos):
+        super().__init__()
+        self.image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "Iron Man", image_file)).convert_alpha()
+        self.image = pygame.transform.scale(self.image, size)
+        self.rect = self.image.get_rect()
+        self.pos = pygame.math.Vector2(pos)
+        self.direction = None
+        self.speed = 500
+
+    def move(self):
+        if self.direction == "left":
+            self.pos.x -= self.speed * Settings.DELTATIME
+        elif self.direction == "right":
+            self.pos.x += self.speed * Settings.DELTATIME
+        elif self.direction == "up":
+            self.pos.y -= self.speed * Settings.DELTATIME
+        elif self.direction == "down":
+            self.pos.y += self.speed * Settings.DELTATIME
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right > Settings.WINDOW.width:
+                self.rect.right = Settings.WINDOW.width
+
+        if self.rect.top < 0:
+            self.rect.top = 0
+        elif self.rect.bottom > Settings.WINDOW.height:
+            self.rect.bottom = Settings.WINDOW.height
+            
+        self.direction = None
+
+    def update(self):
+        self.move()
+        self.rect.topleft = self.pos
+
+
+class Game():
+    def __init__(self):
+        os.environ["SDL_VIDEO_WINDOW_POS"] = "10, 50"
+        pygame.init()
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        pygame.display.set_caption("Iron Defender")
+        self.clock = pygame.time.Clock()
+
+        self.iron_man = pygame.sprite.GroupSingle(Iron_Man("normal.png",(150, 240),(Settings.start_pos)))
+
+        self.background_image = pygame.image.load(os.path.join(Settings.IMAGE_PATH, "background.png")).convert()
+        self.background_image = pygame.transform.scale(self.background_image, self.screen.get_size())
+
+        self.running = True
+
+    def run(self):
+        while self.running:
+            self.watch_for_events()
+            self.update()
+            self.draw()
+            self.clock.tick(Settings.FPS)
+        pygame.quit()
+
+    def draw(self):
+        self.screen.blit(self.background_image)
+        self.iron_man.draw(self.screen)
+        pygame.display.flip()
+
+    def watch_for_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            elif event.type == pygame.KEYDOWN:          
+                if event.key == pygame.K_ESCAPE:  
+                    self.running = False
+
+                elif event.key == pygame.K_LEFT:
+                    self.iron_man.sprite.direction = "left"
+
+
+    def update(self):
+        self.iron_man.update()
+
+
+
+def main():
+    game = Game()
+    game.run()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
